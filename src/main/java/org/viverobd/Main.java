418d4cb368d47f25987e8196b7772e19b3c25ff0
Brandom Galder Hernandez Franco <galderhernandez571@gmail.com>
2026-09-22 12:16:33 -0600

    Cambio al nombre del paquete raíz

--- src/main/java/org/viverobd/Main.java
+++ src/main/java/org/viverobd/Main.java
@@ -1,10 +1,10 @@
-package org.vivero;
+package org.viverobd;
 
 import jakarta.persistence.*;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("./db/vivero.odb");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("./db/viverobd.odb");
         EntityManager em = emf.createEntityManager();
 
         if (em.isOpen()) {
